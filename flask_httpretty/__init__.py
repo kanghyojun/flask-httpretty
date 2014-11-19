a5629815a2da0773c4016789e6ef09112dc990da
Kang Hyojun <hyojun@admire.kr>
2014-11-20 01:52:23 +0900

    Fix flask_httpretty.disable link to enable

--- flask_httpretty/__init__.py
+++ flask_httpretty/__init__.py
@@ -2,5 +2,5 @@ from .core import flaskhttpretty, flaskhttprettified
 
 register_app = flaskhttpretty.register_app
 enable = flaskhttpretty.enable
-disable = flaskhttpretty.enable
+disable = flaskhttpretty.disable
 activate = flaskhttprettified
